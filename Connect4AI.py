092c63cbc581513b341fec5ac134c9a28b8d7db1
Rampagy <ajsimon@mtu.edu>
2018-01-20 12:32:42 -0600

    Added game over after 2 invalid moves in a row

--- Connect4AI.py
+++ Connect4AI.py
@@ -10,39 +10,35 @@ def ComputerPlayer(board_state):
         connect4_classifier = tf.estimator.Estimator(model_fn=c4c.cnn_model_fn, model_dir="Connect4_model/")
 
         # reshape the board_state
-        board_state = np.array(board_state.flatten(), dtype=np.float32, ndmin=2)
+        board_state = np.array(board_state, dtype=np.float32, ndmin=2)
 
         # input the data
-        pred_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": board_state}, num_epochs=1, shuffle=False)
+        pred_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": board_state.flatten()}, num_epochs=1, shuffle=False)
 
         # run the prediction
         eval_results = connect4_classifier.predict(input_fn=pred_input_fn)
 
         # grab the probabilities
         position_probabilities = next(eval_results)['probabilities']
+        #print(position_probabilities)
+        move = 0
+        prev_prob = 0
+        col_count = 0
 
-        # find the highest probability that is also valid
-        move = -1
+        # pick a random number between 0 and 1
+        uniform_num = np.random.uniform(0, 1)
 
-        while (move == -1):
-            prev_prob = 0
-            col_count = 0
+        # check to see which 'window' the random number is in
+        # if the value is in an invalid window (aka invalid move)
+        # pick a new random number and try again until a valid move is chosen
+        for column_prob in position_probabilities:
+            if ((prev_prob <= uniform_num) and \
+                (uniform_num <= prev_prob + column_prob)):
+                    move = col_count
+                    break
 
-            # pick a random number between 0 and 1
-            uniform_num = np.random.uniform()
-
-            # check to see which 'window' the random number is in
-            # if the value is in an invalid window (aka invalid move)
-            # pick a new random number and try again until a valid move is chosen
-            for column_prob in position_probabilities:
-                if ((prev_prob <= uniform_num) and \
-                    (uniform_num <= prev_prob + column_prob) and \
-                    (board_state[0, col_count] == 0)):
-                        move = col_count
-                        break
-
-                prev_prob +=column_prob
-                col_count += 1
+            prev_prob += column_prob
+            col_count += 1
 
         return move
 
@@ -52,3 +48,47 @@ def ComputerPlayer(board_state):
         while (board_state[0, move] != 0):
             move = np.random.randint(board_state.shape[1])
         return move
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def ace_ventura():
+    pass
