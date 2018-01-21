5b77e0132489c67f438601d8f714ba3f3b8d7889
Rampagy <ajsimon@mtu.edu>
2018-01-21 12:03:51 -0600

    Created class for training rather than individual functions

--- Connect4AI.py
+++ Connect4AI.py
@@ -1,53 +1,72 @@
-import Connect4Classifier as c4c
+#import Connect4Classifier as c4c
 import numpy as np
 import tensorflow as tf
 
 
-def ComputerPlayer(board_state):
-    # use a trained model if possible
-    if tf.train.latest_checkpoint('Connect4_model/') != None:
-        # Create the Estimator
-        connect4_classifier = tf.estimator.Estimator(model_fn=c4c.cnn_model_fn, model_dir="Connect4_model/")
+tf.logging.set_verbosity(tf.logging.INFO)
 
-        # reshape the board_state
-        board_state = np.array(board_state, dtype=np.float32, ndmin=2)
 
-        # input the data
-        pred_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": board_state.flatten()}, num_epochs=1, shuffle=False)
+class Connect4Bot():
+    def __init__(self):
+        # create the computational graph
+        self.c4_estimator = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="Connect4_model/")
 
-        # run the prediction
-        eval_results = connect4_classifier.predict(input_fn=pred_input_fn)
 
-        # grab the probabilities
-        position_probabilities = next(eval_results)['probabilities']
-        #print(position_probabilities)
-        move = 0
-        prev_prob = 0
-        col_count = 0
+    def predict_move(self, board_state):
+        # use a trained model if possible
+        if tf.train.latest_checkpoint('Connect4_model/') != None:
+            # reshape the board_state
+            board_state = np.array(board_state, dtype=np.float32, ndmin=2)
 
-        # pick a random number between 0 and 1
-        uniform_num = np.random.uniform(0, 1)
+            # input the data
+            pred_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": board_state.flatten()}, num_epochs=1, shuffle=False)
 
-        # check to see which 'window' the random number is in
-        # if the value is in an invalid window (aka invalid move)
-        # pick a new random number and try again until a valid move is chosen
-        for column_prob in position_probabilities:
-            if ((prev_prob <= uniform_num) and \
-                (uniform_num <= prev_prob + column_prob)):
-                    move = col_count
-                    break
+            # run the prediction
+            eval_results = self.c4_estimator.predict(input_fn=pred_input_fn)
 
-            prev_prob += column_prob
-            col_count += 1
+            # grab the probabilities
+            position_probabilities = next(eval_results)['probabilities']
 
-        return move
+            move = 0
+            prev_prob = 0
+            col_count = 0
 
-    # else pick randomly
-    else:
-        move = np.random.randint(board_state.shape[1])
-        while (board_state[0, move] != 0):
+            # pick a random number between 0 and 1
+            uniform_num = np.random.uniform(0, 1)
+
+            # check to see which 'window' the random number is in
+            # if the value is in an invalid window (aka invalid move)
+            # pick a new random number and try again until a valid move is chosen
+            for column_prob in position_probabilities:
+                if ((prev_prob <= uniform_num) and \
+                    (uniform_num <= prev_prob + column_prob)):
+                        move = col_count
+                        break
+
+                prev_prob += column_prob
+                col_count += 1
+
+            return move
+
+        # else pick randomly
+        else:
             move = np.random.randint(board_state.shape[1])
-        return move
+            while (board_state[0, move] != 0):
+                move = np.random.randint(board_state.shape[1])
+            return move
+
+
+    def train_game(self, features, labels):
+        # Train the model
+        train_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x={"x": features},
+            y=labels,
+            batch_size=100,
+            num_epochs=None,
+            shuffle=True)
+        self.c4_estimator.train(
+            input_fn=train_input_fn,
+            steps=1)
 
 
 
@@ -56,6 +75,124 @@ def ComputerPlayer(board_state):
 
 
 
+"""Convolutional Neural Network Estimator for connect4, built with tf.layers."""
+
+def cnn_model_fn(features, labels, mode):
+  """Model function for CNN."""
+  # Input Layer
+  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+  # connect4 'images' are 10x10 pixels, and have one color channel
+  input_layer = tf.reshape(features["x"], [-1, 10, 10, 1])
+
+  # Convolutional Layer #1
+  # Computes 32 features using a 2x2 filter with ReLU activation.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 10, 10, 1]
+  # Output Tensor Shape: [batch_size, 10, 10, 32]
+  conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=32,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Convolutional Layer #2
+  # Computes 64 features using a 2x2 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 10, 10, 32]
+  # Output Tensor Shape: [batch_size, 10, 10, 64]
+  conv2 = tf.layers.conv2d(
+      inputs=conv1,
+      filters=64,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Convolutional Layer #2
+  # Computes 64 features using a 2x2 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 10, 10, 32]
+  # Output Tensor Shape: [batch_size, 10, 10, 64]
+  conv3 = tf.layers.conv2d(
+      inputs=conv2,
+      filters=96,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Convolutional Layer #2
+  # Computes 64 features using a 2x2 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 10, 10, 32]
+  # Output Tensor Shape: [batch_size, 10, 10, 64]
+  conv4 = tf.layers.conv2d(
+      inputs=conv3,
+      filters=128,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Convolutional Layer #2
+  # Computes 64 features using a 2x2 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 10, 10, 32]
+  # Output Tensor Shape: [batch_size, 10, 10, 64]
+  conv5 = tf.layers.conv2d(
+      inputs=conv4,
+      filters=160,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Flatten tensor into a batch of vectors
+  # Input Tensor Shape: [batch_size, 10, 10, 64]
+  # Output Tensor Shape: [batch_size, 10 * 10 * 64]
+  conv2_flat = tf.reshape(conv5, [-1, 10 * 10 * 160])
+
+  # Dense Layer
+  # Densely connected layer with 1024 neurons
+  # Input Tensor Shape: [batch_size, 10 * 10 * 64]
+  # Output Tensor Shape: [batch_size, 1024]
+  dense = tf.layers.dense(inputs=conv2_flat, units=2048, activation=tf.nn.relu)
+
+  # Add dropout operation; 0.6 probability that element will be kept
+  dropout = tf.layers.dropout(
+      inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+  # Logits layer
+  # Input Tensor Shape: [batch_size, 1024]
+  # Output Tensor Shape: [batch_size, 10]
+  logits = tf.layers.dense(inputs=dropout, units=10)
+
+  predictions = {
+      # Generate predictions (for PREDICT and EVAL mode)
+      "classes": tf.argmax(input=logits, axis=1),
+      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+      # `logging_hook`.
+      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+  }
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+  # Calculate Loss (for both TRAIN and EVAL modes)
+  onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
+  loss = tf.losses.softmax_cross_entropy(
+      onehot_labels=onehot_labels, logits=logits)
+
+  # Configure the Training Op (for TRAIN mode)
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.00001)
+    train_op = optimizer.minimize(
+        loss=loss,
+        global_step=tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+  # Add evaluation metrics (for EVAL mode)
+  eval_metric_ops = {
+      "accuracy": tf.metrics.accuracy(
+          labels=labels, predictions=predictions["classes"])}
+  return tf.estimator.EstimatorSpec(
+      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
 
--- TrainBot.py
+++ TrainBot.py
@@ -3,13 +3,13 @@ import HumanPlayer as hp
 import numpy as np
 import Connect4AI as c4a
 import tensorflow as tf
-import Connect4Classifier as c4c
 import math
+import time
 
 # Call this function to play a game of connect4
 # Returns a game log of the winning players moves
 # the winning players pieces are always represented as a 1
-def TrainBot():
+def TrainBot(aiBot):
     # create instance of the game
     game = c4.connect4()
     game_complete = False
@@ -25,12 +25,12 @@ def TrainBot():
             if players_turn == 0:
                 # player 0's pieces are represented as 1, so don't do anything
                 # because his pieces are already from his perspective
-                move = c4a.ComputerPlayer(np.copy(game.board))
+                move = aiBot.predict_move(np.copy(game.board))
                 #move = hp.HumanAI(np.copy(game.board)*-1)
             else:
                 # player 1's pieces are represented as 2, so flip his pieces
                 # so that from every players perspective their pieces are 1
-                move = c4a.ComputerPlayer(FlipPlayers(np.copy(game.board)))
+                move = aiBot.predict_move(FlipPlayers(np.copy(game.board)))
                 #move = hp.HumanAI(FlipPlayers(np.copy(game.board)))
 
             valid_input, player_won, game_complete = game.AddMove(move)
@@ -76,10 +76,13 @@ def FlipPlayers(board_state):
 
 
 
+# create AI class
+aiBot = c4a.Connect4Bot()
 
-for game_num in range(20000):
-    player_winner, c4game_log = TrainBot()
-    print(game_num)
+for game_num in range(10):
+    game_time_start = time.process_time()
+    player_winner, c4game_log = TrainBot(aiBot)
+    game_time = time.process_time() - game_time_start
 
     # do back propagation if a player won
     if player_winner:
@@ -88,10 +91,6 @@ for game_num in range(20000):
 
         # init array for labels
         truth_labels = np.zeros(shape=(2*len(c4game_log)), dtype=np.float32)
-
-        # Create the Estimator
-        connect4_classifier = tf.estimator.Estimator(
-            model_fn=c4c.cnn_model_fn, model_dir="Connect4_model/")
 
         # go through each board state and add it to the appropriate arrays
         for i in range(2*len(c4game_log)):
@@ -110,17 +109,11 @@ for game_num in range(20000):
         #for game_disp, truth_disp in zip(game_states, truth_labels):
         #    print("{}\n{}\n\n".format(np.reshape(game_disp, (10, 10)), truth_disp))
 
-        # Train the model
-        train_input_fn = tf.estimator.inputs.numpy_input_fn(
-            x={"x": game_states},
-            y=truth_labels,
-            batch_size=100,
-            num_epochs=None,
-            shuffle=True)
-        connect4_classifier.train(
-            input_fn=train_input_fn,
-            steps=1)
+        train_time_start = time.process_time()
+        aiBot.train_game(game_states, truth_labels)
+        train_time = time.process_time() - train_time_start
 
+        print("{} took {} seconds for the game and {} seconds for the training".format(game_num, game_time, train_time))
 
 
 
